e77410e78ae2d13ba6363082ee397f8a9e2867a0
Eddishot <Eddi_sl94@hotmail.it>
2021-10-13 18:22:13 +0200

    exercise completed

---
+++ js/dadi.js
@@ -0,0 +1,20 @@
+// creo due let. uno mio e uno per il computer
+
+let numeriUtente = (Math.random() * 6);
+console.log (numeriUtente)
+
+let numeriComputer =(Math.random() * 6);
+console.log (numeriComputer)
+
+
+
+// creo un if per fare la comparizione
+
+if( numeriUtente > numeriComputer){
+
+    alert("Complimneti! hai vinto")
+
+} else{
+
+    alert("Mi dispiace. Hai perso")
+}
